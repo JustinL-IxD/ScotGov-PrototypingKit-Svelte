bb98067f9a1b3f5f6007af924a072ad755492d13
Justin <120177475+JustinL-IxD@users.noreply.github.com>
2023-03-28 14:51:57 +0100

    Update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
     import "../app.css"; 
 </script>
 
+<h1>Logo would go here</h1>
 <slot />
+<footer>scottish goverment 2023</footer>
---
+++ src/routes/olu/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import PrimaryButton from "$lib/Buttons/PrimaryButton.svelte";
+    import TextInput from "$lib/Forms/TextInput.svelte";
+	import Body from "$lib/Typography/Body.svelte";
+    import H1 from "$lib/Typography/H1.svelte";
+</script>
+
+<H1>Olu justin test</H1>
+<Body>fdsafadsf</Body>
+<TextInput />
+<PrimaryButton />
