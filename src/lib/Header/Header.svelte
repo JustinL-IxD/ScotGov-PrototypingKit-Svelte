5f378cfb8e6ef89c4054c0b39cdf4a4ba5b02a65
Justin <120177475+JustinL-IxD@users.noreply.github.com>
2023-04-13 14:36:24 +0100

    add logo

--- src/lib/Header/Header.svelte
+++ src/lib/Header/Header.svelte
@@ -9,5 +9,5 @@
 
 
 <div class="w-full h-20 flex items-center justify-start border-b border-gray-[#f8f8f8] mb-6">
-    <img src={logo} alt="Social Security Scotland logo" class="h-9 mt-2 mb-1 object-contain">
+    <img src={logo} alt="Social Security Scotland logo" class="h-9 mt-2 mb-2 object-contain">
 </div>
