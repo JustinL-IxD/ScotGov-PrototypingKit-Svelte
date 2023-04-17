53c22a535edd1b09489c945a7d7fe5ccf7935a3e
Justin <120177475+JustinL-IxD@users.noreply.github.com>
2023-04-17 13:59:36 +0100

    reset repo

--- src/routes/about-user/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import PrimaryButton from "$lib/Buttons/PrimaryButton.svelte";
-	import Body from "$lib/Typography/Body.svelte";
-	import H1 from "$lib/Typography/H1.svelte";
-</script>
-
-<H1>About User</H1>
-<Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Body>
-<PrimaryButton name="Start application" destination="about-benefit" />
