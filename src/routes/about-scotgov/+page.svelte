201456486246d961579de5edb5d1129b83ea87e6
Justin <120177475+JustinL-IxD@users.noreply.github.com>
2023-04-18 11:25:45 +0100

    Iteration 1

---
+++ src/routes/about-scotgov/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Textarea from "$lib/Forms/Textarea.svelte";
+    import H1 from "$lib/Typography/H1.svelte";
+    import SecondaryButton from "$lib/Buttons/SecondaryButton.svelte";
+</script>
+
+
+<H1>About Scot Gov</H1>
+<Textarea />
+<SecondaryButton name="Back" destination="about-user" /> 
---
+++ src/routes/about-user/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import PrimaryButton from "$lib/Buttons/PrimaryButton.svelte";
+import SecondaryButton from "$lib/Buttons/SecondaryButton.svelte";
+import RadioButtons from "$lib/Forms/RadioButtons.svelte";
+import TextInput from "$lib/Forms/TextInput.svelte";
+    import H1 from "$lib/Typography/H1.svelte";
+</script>
+
+<H1>About User</H1>
+<TextInput heading="first name" />
+<RadioButtons option1="Option 1 text" option2="Option 2 text" />
+<SecondaryButton name="Back" destination="about-the-benefit" /> 
+<PrimaryButton name="Continue" destination="about-scotgov" />
